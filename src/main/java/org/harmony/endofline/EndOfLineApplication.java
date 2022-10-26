98f8c1f4cef0a5ae0975d36414ab40947eda6927
kiinami <kinami@posteo.net>
2022-10-26 12:24:12 +0200

    feat: re-initialized the repo

--- src/main/java/org/harmony/endofline/EndOfLineApplication.java
+++ src/main/java/org/harmony/endofline/EndOfLineApplication.java
@@ -1,13 +1,13 @@
-package org.springframework.samples.endofline;
+package org.harmony.endofline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
-public class EndoflineApplication {
+@SpringBootApplication
+public class EndOfLineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EndoflineApplication.class, args);
+		SpringApplication.run(EndOfLineApplication.class, args);
 	}
 
 }
--- src/main/java/org/springframework/samples/endofline/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.samples.endofline.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends  CrudRepository<User, String>{
-
-}
