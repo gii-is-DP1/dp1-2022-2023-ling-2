0e4dac40b38988cf3d615be40d41cd11d7b3fda9
kiinami <kinami@posteo.net>
2022-10-30 09:29:19 +0100

    feat: added configuraiton package

---
+++ src/main/java/org/harmony/endofline/configuration/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package org.harmony.endofline.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/resources/**","/webjars/**","/h2-console/**").permitAll()
+            .antMatchers(HttpMethod.GET, "/","/oups").permitAll()
+            // TODO: Here we should put the security for the rest of the pages
+            .anyRequest().denyAll()
+            .and()
+            .formLogin()
+            /*.loginPage("/login")*/
+            .failureUrl("/login-error")
+            .and()
+            .logout()
+            .logoutSuccessUrl("/");
+        http.csrf().ignoringAntMatchers("/h2-console/**");
+        http.headers().frameOptions().sameOrigin();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery(
+                "select username,password,enabled "
+                    + "from users "
+                    + "where username = ?")
+            .authoritiesByUsernameQuery(
+                "select username, authority "
+                    + "from authorities "
+                    + "where username = ?")
+            .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder =  NoOpPasswordEncoder.getInstance();
+        return encoder;
+    }
+
+}
