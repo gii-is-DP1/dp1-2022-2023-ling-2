c66a1a3424b96c17663beced6d6731af7db39bfe
Vicky Moreno <victoriaemc10@gmail.com>
2022-11-05 17:16:04 +0100

    fix: MockMVC not null
    Pesky imports were to blame

--- src/test/java/org/harmony/endofline/model/MultiplayerTest.java
+++ src/test/java/org/harmony/endofline/model/MultiplayerTest.java
@@ -5,10 +5,8 @@ import org.harmony.endofline.multiplayer.MultiplayerController;
 import org.harmony.endofline.multiplayer.MultiplayerRepository;
 import org.harmony.endofline.multiplayer.MultiplayerService;
 import org.harmony.endofline.user.UserService;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,9 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.beans.factory.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(value = MultiplayerController.class)
@@ -45,7 +43,7 @@ public class MultiplayerTest {
     private void multiplayerLoggedIn() throws Exception {
 
         // TODO everything fine
-        mockMvc.perform(get("/multiplayer/create")).andExpect(status().isOk())
+        mockMvc.perform(post("/multiplayer/create")).andExpect(status().isOk())
             .andExpect(model().attributeExists("game"));
     }
 
