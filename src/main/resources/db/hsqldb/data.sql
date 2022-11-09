597c1c9ce945636358b83c0a20f57fd67fa664e1
Antonio Rodriguez Ruiz <antoniorodriguezruiz2002@gmail.com>
2022-11-09 11:27:45 +0100

    feat: Game creation in data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -11,3 +11,8 @@ INSERT INTO cards(initiative,top_side,bottom_side,left_side,right_side) VALUES (
 
 INSERT INTO users(username,password,email,is_admin,enabled) VALUES ('admin','admin','admin@localhost.com',true,true);
 INSERT INTO users(username,password,email,is_admin,enabled) VALUES ('user','user','user@localhost.com',false,true);
+
+INSERT INTO multiplayer_games(date_ended, date_started, p1energy_left, p2energy_left) VALUES (null, '2022-11-09 11:08:21.139157', 3, 3);
+INSERT INTO usergames(user_id, game_id, player, role) VALUES (1, 1, 1, 'player');
+INSERT INTO usergames(user_id, game_id, player, role) VALUES (2, 1, 2, 'player');
+INSERT INTO singleplayer_game(date_ended, date_started, user_id) VALUES (null, '2022-11-09 11:08:21.139157', 1);
