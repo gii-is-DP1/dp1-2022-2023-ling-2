74df66f4000fd453015e407d35adba868087138f
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-12-01 18:48:43 +0100

    test: Retrieve singleplayer cards

--- src/test/java/org/harmony/endofline/singleplayer/SingleplayerServiceTest.java
+++ src/test/java/org/harmony/endofline/singleplayer/SingleplayerServiceTest.java
@@ -1,5 +1,6 @@
 package org.harmony.endofline.singleplayer;
 
+import org.harmony.endofline.gameCard.GameCard;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -22,4 +24,20 @@ public class SingleplayerServiceTest {
         List<Singleplayer> games = singleplayerService.getAllGamesWithUser();
         assertThat(games).hasSize(1);
     }
+
+    @Test
+    void shouldGiveAllHandCardsOfGameOne(){
+        List<GameCard> cards = singleplayerService.getAllCardsInHand(1);
+        assertEquals(cards.size(), 1);
+        assertThat(cards.stream().allMatch(c -> c.getCard().getId()==3));
+    }
+
+    @Test
+    void shouldGiveAllBoardCardsOfGameOne(){
+        List<GameCard> cards = singleplayerService.getAllCardsInBoard(1);
+        assertEquals(cards.size(), 1);
+        assertThat(cards.stream().allMatch(c -> c.getCard().getId()==3));
+    }
+
+
 }
