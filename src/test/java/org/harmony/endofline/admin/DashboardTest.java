e21923dc7ce91a9d437b3fb3d989b4c8feeb2c69
antonio <antoniorodriguezruiz2002@gmail.com>
2022-12-17 20:18:08 +0100

    refactor: Remove unnecessary decorators

--- src/test/java/org/harmony/endofline/admin/DashboardTest.java
+++ src/test/java/org/harmony/endofline/admin/DashboardTest.java
@@ -32,8 +32,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext
+@SpringBootTest()
 public class DashboardTest {
 
     private MockMvc mockMvc;
